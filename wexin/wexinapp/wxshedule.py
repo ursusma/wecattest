0bf1df841d628e0b97149fc17147a3ebc8befedd
ursusma <736324424@qq.com>
2016-11-24 18:06:23 +0800

    Update wxshedule.py

--- wexin/wexinapp/wxshedule.py
+++ wexin/wexinapp/wxshedule.py
@@ -5,8 +5,8 @@ import requests
 import json
 import os
 
-AppID = 'wxb957e3605e03b369'
-AppSecret = 'a2445b89eaf6f07b8cc3699069e2a768'
+AppID = ''
+AppSecret = ''
 
 config_get_access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (AppID, AppSecret)
 
